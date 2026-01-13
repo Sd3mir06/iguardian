63f97e075c61e4b7d21c3981d4d44c6694901dc3
Sukru Demir <Sukru.demir@outlook.com.tr>
2026-01-13 23:15:26 +0300

    Initial Commit

---
+++ iguardian/iguardianApp.swift
@@ -0,0 +1,17 @@
+//
+//  iguardianApp.swift
+//  iguardian
+//
+//  Created by Sukru Demir on 13.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct iguardianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
